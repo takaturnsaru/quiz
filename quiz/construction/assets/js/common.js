eb95da4c8e9ade78bfb1eb11f7e000b7f2c8b0ee
takaturnsaru <69668280+takaturnsaru@users.noreply.github.com>
2023-05-19 05:59:34 +0900

    土台完了

--- quiz/construction/assets/js/common.js
+++ quiz/construction/assets/js/common.js
@@ -15,7 +15,12 @@ $(function () {
 
 const question = document.getElementById('question');
 const btns = document.querySelectorAll('.choice');
-let quizzIndex = 0;
+const nextBtn = document.getElementById('next-btn');
+const redoBtn = document.getElementById('redo-btn');
+const resultContent = document.getElementById('result-content');
+let quizIndex = 0;
+let correctIndex = 0;
+let  quizNum= 0;
 
 const quizzes = [
   {
@@ -33,12 +38,42 @@ const quizzes = [
     choices: ['h', 'i', 'j', 'k'],
     answer: 'k'    
   },  
+  {
+    question: 'Q4',
+    choices: ['l', 'm', 'n', 'o'],
+    answer: 'n'    
+  },  
+  {
+    question: 'Q5',
+    choices: ['p', 'q', 'r', 's'],
+    answer: 'q'    
+  },  
+  {
+    question: 'Q6',
+    choices: ['t', 'u', 'v', 'w'],
+    answer: 'v'    
+  },  
+  {
+    question: 'Q7',
+    choices: ['x', 'y', 'z', '1'],
+    answer: 'z'    
+  },  
+  {
+    question: 'Q8',
+    choices: ['2', '3', '4', '5'],
+    answer: '3'    
+  },  
+  {
+    question: 'Q9',
+    choices: ['6', '7', '8', '9'],
+    answer: '8'    
+  },  
 ];
 
 function quizSet() {
-  question.textContent = quizzes[quizzIndex].question;
+  question.textContent = quizzes[quizIndex].question;
   for(let i = 0; i < btns.length; i++){
-    btns[i].textContent = quizzes[quizzIndex].choices[i];
+    btns[i].textContent = quizzes[quizIndex].choices[i];
   }
 }
 
@@ -46,13 +81,64 @@ quizSet();
 
 btns.forEach(btn => {
   btn.addEventListener('click', () => {
-    if(btn.textContent === quizzes[quizzIndex].answer){
+    if(btn.textContent === quizzes[quizIndex].answer){
       btn.classList.add('correct');
+      correctIndex++;
     }else{
       btn.classList.add('lose');
     }
-    quizSet();
+    nextBtn.classList.remove('hidden');
   })
 })
+
+function classRemove() {
+  btns.forEach(btn => {
+    btn.classList.remove('correct');
+    btn.classList.remove('lose');
+  })
+}
+
+// let len = quizzes.length;
+// console.log(len);
+
+// function quizRandam() {
+//   for(len > 1; len--;){
+//     let num = Math.floor(Math.random() * len);
+//     let k = quizzes[len];
+//     quizzes[len] = quizzes[num];
+//     quizzes[num] = k;
+//   }
+// }
+
+
+
+
+nextBtn.addEventListener('click', () => {
+  if(quizNum === 5){
+    overlay.classList.remove('hidden');
+    result.classList.remove('hidden');
+    resultContent.textContent = `${quizNum}問中${correctIndex}問正解でした!!`
+    classRemove();
+  }else{
+    quizNum++;
+    quizRandam();
+    console.log(quizIndex);
+    classRemove();
+    nextBtn.classList.add('hidden');
+    quizSet();
+  }
+})
+
+redoBtn.addEventListener('click', () => {
+  quizIndex = 0;
+  overlay.classList.add('hidden');
+  result.classList.add('hidden');
+  nextBtn.classList.add('hidden');
+  quizSet();
+})
+
+
+
+
 
 
